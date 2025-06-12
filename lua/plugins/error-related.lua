eb3ad6d3507fa717a2dccbf9c12893da5fdd5a34
zyc-sudo <zyctc000@gmail.com>
2025-06-11 20:06:07 -0400

    added some error ignore for ale python

--- lua/plugins/error-related.lua
+++ lua/plugins/error-related.lua
@@ -1,5 +1,12 @@
-return{
-  {"nvie/vim-flake8"},
-  {"tell-k/vim-autopep8"},
-  {"dense-analysis/ale"}
+return {
+  { "nvie/vim-flake8" },
+  { "tell-k/vim-autopep8" },
+  {
+    "dense-analysis/ale",
+    config = function()
+      vim.g.ale_python_pylint_options = "--max-line-length=120 --disable=R0902,C0103,C0111"
+      vim.g.ale_python_flake8_options =
+        "--max-line-length=120 --disable=R0902,C0103,C0111 --extension-pkg-whitelist=cv2 --generated-members=numpy.*,torch.*,cv2.*,Cython.*,onxx.*"
+    end,
+  },
 }
